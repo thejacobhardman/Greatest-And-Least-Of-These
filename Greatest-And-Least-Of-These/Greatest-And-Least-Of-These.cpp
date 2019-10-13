76365ac82c87e5db541331e863776e0a5a693ec0
Jacob Hardman <42230736+thejacobhardman@users.noreply.github.com>
2019-10-13 13:06:10 -0600

    Add project files.

---
+++ Greatest-And-Least-Of-These/Greatest-And-Least-Of-These.cpp
@@ -0,0 +1,24 @@
+// Jacob Hardman
+// Comp Sci 1
+// Professor Jennifer Bailey
+// 10/13/19
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	bool isRunning = true, userConfirm = false;
+	string userInput = "";
+	int minNum, maxNum;
+
+	cout << "Enter a series of integers and I will tell you which one is the largest and the smallest." << endl;
+	cout << "Enter '-99' to exit the program and display your results." << endl;
+
+	while (isRunning) {
+
+		while (userConfirm == false);
+	}
+}
+
